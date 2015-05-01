95968f64bd4453d83ea193a224ea51f248d8387a
dichika <ichikawa@hcc-jp.com>
2015-05-01 16:21:49 +0900

    bug fix

--- R/faceplusplus.R
+++ R/faceplusplus.R
@@ -5,12 +5,12 @@ getDetect <- function(file, secret=options()$FACEPP_SECRET, key=options()$FACEPP
     u <- sprintf("https://apius.faceplusplus.com/v2/detection/detect?url=%s&api_secret=%s&api_key=%s&attribute=glass,pose,gender,age,race,smiling",
                  file,secret,key
     )
-    res <- GET(u)
+    res <- httr::GET(u)
   } else{
     u <- sprintf("https://apius.faceplusplus.com/v2/detection/detect?api_secret=%s&api_key=%s&attribute=glass,pose,gender,age,race,smiling",
                  secret, key
     )
-    res <- POST(u, body=list(img=upload_file(file)))    
+    res <- httr::POST(u, body=list(img=httr::upload_file(file)))    
   }
   res <- content(res, as="text")
   res <- jsonlite::fromJSON(res)
